94aab8bbb634112d820a0b8537a62684b1c7d756
Salmon <yshpeng@liangzhicn.com>
2020-10-30 13:44:28 +0800

    player script and shell script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("MyGame/Player")]
+public class Player : MonoBehaviour
+{
+    public float m_speed = 1;
+    private Transform m_transform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_transform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //縱向移動距離
+        float movev = 0;
+        //水平移動距離
+
+        float moveh = 0;
+
+        //按上鍵Z方向遞增
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movev += m_speed * Time.deltaTime;
+        }
+
+        //按下鍵Z方向遞減
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movev -= m_speed * Time.deltaTime;
+        }
+
+        //按左鍵X方向遞減
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveh -= m_speed * Time.deltaTime;
+        }
+
+        //按右鍵X方向遞增
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveh += m_speed * Time.deltaTime;
+        }
+
+        //移動
+        m_transform.Translate(new Vector3(moveh, 0, movev));
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("MyGame/Rocket")]
+public class Rocket : MonoBehaviour
+{
+    public float m_speed = 10;   // 子彈飛行速度
+    public float m_power = 1.0f; // 威力
+
+    private void OnBecameInvisible()  // 當離開屏幕 無法被渲染時 自動調用
+    {
+        if (this.enabled)  //通過判斷當前子彈是否處於激活狀態 防止重複刪除
+        {
+            Destroy(this.gameObject); // 刪除子彈
+        }
+    }
+
+    private void OnBecameVisible()
+    {
+        Debug.Log("我是子彈君");
+    }
+}
